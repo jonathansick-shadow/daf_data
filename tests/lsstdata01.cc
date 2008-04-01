dceffae31c532d636100d390252d691b92ba127b
rowen <rowen@git.lsstcorp.org>
2008-04-01 00:24:06 +0000

    daf_data reorg: - Updated the SConscript files and ups table files

--- tests/lsstdata01.cc
+++ tests/lsstdata01.cc
@@ -17,12 +17,12 @@
 // 
 // lsstdata01.cc demonstrates specialization on LsstBase, 
 // object creation and configuration, exercising the factories, 
-// the required fw includes, use of trace verbosity to control debug 
+// the required lsst/daf/data includes, use of trace verbosity to control debug 
 // output
 //
 // This program should be built by scons when run from the overlying
 //    directory.
-// Run this program to do a sanity check on the fw classes. 
+// Run this program to do a sanity check on the lsst::daf::data classes. 
 // Examine the output of the program for the following:
 // 1) Proper pairing of constructors/destructors, 
 // 2) Only one initialization of singleton classes SupportFactory and
@@ -31,7 +31,7 @@
 // 4) The program should run with no errors. 
 // 5) You can specify a trace verbosity level with the command line (default = 100)
 // 
-// With that check out of the way, you are ready to use the fw classes to 
+// With that check out of the way, you are ready to use the lsst::daf::data classes to 
 // develop your LsstBase derivations and application code
 //
 // SAMPLE OUTPUT (verbosity=100):
@@ -42,40 +42,40 @@
 //                     Exit SupportFactory::SupportFactory()
 //                     Enter SupportFactory::createPolicy()
 //                     Enter Policy::Policy()
-//                     Exit Policy::Policy() : 1: 0x985454c lsst::fw::Policy
+//                     Exit Policy::Policy() : 1: 0x985454c lsst::pex::policy::Policy
 //                     Exit SupportFactory::createPolicy()
-// Done: Created policy object '1: 0x985454c lsst::fw::Policy'
+// Done: Created policy object '1: 0x985454c lsst::pex::policy::Policy'
 // Creating an LsstData realization
 //                     Enter LsstImpl_DC2::LsstImpl_DC2(P6MyLsst)
 //                     Enter SupportFactory::createMetadata()
 //                     Enter Metadata::Metadata()
-//                     Exit Metadata::Metadata() : 3: 0x98545bc lsst::fw::Metadata
+//                     Exit Metadata::Metadata() : 3: 0x98545bc lsst::daf::data::Metadata
 //                     Exit SupportFactory::createMetadata()
 //                     Enter SupportFactory::createPersistence()
 //                     Enter Persistence::Persistence()
-//                     Exit Persistence::Persistence() : 4: 0x98548ec lsst::fw::Persistence
+//                     Exit Persistence::Persistence() : 4: 0x98548ec lsst::daf::data::Persistence
 //                     Exit SupportFactory::createPersistence()
 //                     Enter SupportFactory::createPolicy()
 //                     Enter Policy::Policy()
-//                     Exit Policy::Policy() : 5: 0x98547bc lsst::fw::Policy
+//                     Exit Policy::Policy() : 5: 0x98547bc lsst::pex::policy::Policy
 //                     Exit SupportFactory::createPolicy()
 //                     Enter SupportFactory::createProvenance()
 //                     Enter Provenance::Provenance()
-//                     Exit Provenance::Provenance() : 6: 0x9854614 lsst::fw::Provenance
+//                     Exit Provenance::Provenance() : 6: 0x9854614 lsst::daf::data::Provenance
 //                     Exit SupportFactory::createProvenance()
 //                     Enter SupportFactory::createReleaseProcess()
 //                     Enter ReleaseProcess::ReleaseProcess()
-//                     Exit ReleaseProcess::ReleaseProcess() : 7: 0x985457c lsst::fw::ReleaseProcess
+//                     Exit ReleaseProcess::ReleaseProcess() : 7: 0x985457c lsst::daf::data::ReleaseProcess
 //                     Exit SupportFactory::createReleaseProcess()
 //                     Enter SupportFactory::createSecurity()
 //                     Enter Security::Security()
-//                     Exit Security::Security() : 8: 0x9854714 lsst::fw::Security
+//                     Exit Security::Security() : 8: 0x9854714 lsst::daf::data::Security
 //                     Exit SupportFactory::createSecurity()
 //                     Exit LsstImpl_DC2::LsstImpl_DC2() : 2: 0x9854664 MyLsst
 //           In MyLsst::MyLsst(An LsstData realization)
 //  Done: Created MyLsst object '2: 0x936f754 MyLsst'
 //  Configuring the LsstData object
-//                     LsstDataConfigurator::configureSupport(1: 0x985454c lsst::fw::Policy)
+//                     LsstDataConfigurator::configureSupport(1: 0x985454c lsst::pex::policy::Policy)
 //  Configured MyLsst object '2: 0x9854664 MyLsst'
 //                     Enter LsstImpl_DC2::getChildren(1)
 //                     Exit LsstImpl_DC2::getChildren()
@@ -84,21 +84,21 @@
 //           MyLsst::~MyLsst()
 //                     Enter LsstImpl_DC2::~LsstImpl_DC2() : 2: 0x9854664 MyLsst
 //                     Exit LsstImpl_DC2::~LsstImpl_DC2()
-//                     Enter Security::~Security() : 8: 0x9854714 lsst::fw::Security
+//                     Enter Security::~Security() : 8: 0x9854714 lsst::daf::data::Security
 //                     Exit Security::~Security()
-//                     Enter ReleaseProcess::~ReleaseProcess() : 7: 0x985457c lsst::fw::ReleaseProcess
+//                     Enter ReleaseProcess::~ReleaseProcess() : 7: 0x985457c lsst::daf::data::ReleaseProcess
 //                     Exit ReleaseProcess::~ReleaseProcess()
-//                     Enter Provenance::~Provenance() : 6: 0x9854614 lsst::fw::Provenance
+//                     Enter Provenance::~Provenance() : 6: 0x9854614 lsst::daf::data::Provenance
 //                     Exit Provenance::~Provenance()
-//                     Enter Policy::~Policy() : 5: 0x98547bc lsst::fw::Policy
+//                     Enter Policy::~Policy() : 5: 0x98547bc lsst::pex::policy::Policy
 //                     Exit Policy::~Policy()
-//                     Enter Persistence::~Persistence() : 4: 0x98548ec lsst::fw::Persistence
+//                     Enter Persistence::~Persistence() : 4: 0x98548ec lsst::daf::data::Persistence
 //                     Exit Persistence::~Persistence()
-//                     Enter Metadata::~Metadata() : 3: 0x98545bc lsst::fw::Metadata
+//                     Enter Metadata::~Metadata() : 3: 0x98545bc lsst::daf::data::Metadata
 //                     Exit Metadata::~Metadata()
 //  Done destroying MyLsst object
-//  Policy object '1: 0x985454c lsst::fw::Policy' going out of scope
-//                     Enter Policy::~Policy() : 1: 0x985454c lsst::fw::Policy
+//  Policy object '1: 0x985454c lsst::pex::policy::Policy' going out of scope
+//                     Enter Policy::~Policy() : 1: 0x985454c lsst::pex::policy::Policy
 //                     Exit Policy::~Policy()
 //  No leaks detected
 //
@@ -128,11 +128,11 @@ public:
 
 MyLsst::MyLsst(string s) : LsstBase(typeid(this)) 
 { 
-	Trace( "fw.MyLsst", 10, boost::format("In MyLsst::MyLsst(%s)") % s);
+	Trace("lsstdata01.MyLsst", 10, boost::format("In MyLsst::MyLsst(%s)") % s);
 }
 MyLsst::~MyLsst()
 {
-	Trace( "fw.MyLsst", 10, "MyLsst::~MyLsst()");
+	Trace("lsstdata01.MyLsst", 10, "MyLsst::~MyLsst()");
 }
 
 int main( int argc, char** argv )
@@ -157,24 +157,24 @@ int main( int argc, char** argv )
         // Create a free Policy object to use for LsstData object 
         //    configuration.
         //
-        Trace( "lsstdata01",1, "Explicitly creating a policy object");
+        Trace("lsstdata01", 1, "Explicitly creating a policy object");
         lsst::pex::policy::Policy::Ptr sp = lsst::pex::policy::Policy::Ptr(
             new lsst::pex::policy::Policy());
-        Trace( "lsstdata01",1, 
+        Trace("lsstdata01", 1, 
             boost::format("Created policy object '%s'") % sp.get()->toString());
          
         //
         // Create a new instance of an LsstData realization
         // Configure the instance via a given Policy object
         //
-        Trace( "lsstdata01",1, "Creating an LsstData realization");
+        Trace("lsstdata01", 1, "Creating an LsstData realization");
         MyLsst::PtrType x = MyLsst::PtrType(new MyLsst( "An LsstData realization" ));
-        Trace( "lsstdata01",1, 
+        Trace("lsstdata01", 1, 
             boost::format("Done: Created MyLsst object '%s'") % x->toString());
 
-        Trace( "lsstdata01",1, "Configuring the LsstData object");
+        Trace("lsstdata01", 1, "Configuring the LsstData object");
         LsstDataConfigurator::configureSupport(x,sp);
-        Trace( "lsstdata01",1, 
+        Trace("lsstdata01", 1, 
             boost::format("Done: Configured MyLsst object '%s'") % x->toString());
 
         //
@@ -184,15 +184,13 @@ int main( int argc, char** argv )
         
         LsstData::IteratorRangeType range = x->getChildren();
         if( std::distance( range.first, range.second) != 0 ) {
-            Trace( 
-                "lsstdata01", 1,
-                    "Error: MyLsst object is a simple object and does not " \
-                    "collect children. getChildren should return an iterator range of 0");
+            Trace("lsstdata01", 1,
+                "Error: MyLsst object is a simple object and does not " \
+                "collect children. getChildren should return an iterator range of 0");
         }
         else {
-            Trace( 
-                "lsstdata01", 1,
-                    "OK: MyLsst::getChildren returned an interator range of 0");
+            Trace("lsstdata01", 1,
+                "OK: MyLsst::getChildren returned an interator range of 0");
         }
             
  
@@ -201,10 +199,10 @@ int main( int argc, char** argv )
         // all of the base class' destructors and generate trace output 
         //          
 
-        Trace( "lsstdata01",1, 
+        Trace("lsstdata01", 1, 
             boost::format("MyData object '%s' going out of scope") \
                 % x->toString());
-        Trace( "lsstdata01",1, 
+        Trace("lsstdata01", 1, 
             boost::format("Policy object '%s' going out of scope") \
                 % sp->toString());
     }
@@ -212,9 +210,9 @@ int main( int argc, char** argv )
     // Check for memory leaks
     //
     if (lsst::daf::base::Citizen::census(0) == 0) {
-        Trace( "lsstdata01",1, "No leaks detected");
+        Trace("lsstdata01", 1, "No leaks detected");
     } else {
-        Trace( "lsstdata01",1, "ERROR: Memory leaks detected!");
+        Trace("lsstdata01", 1, "ERROR: Memory leaks detected!");
         lsst::daf::base::Citizen::census(cerr);
     }
 
