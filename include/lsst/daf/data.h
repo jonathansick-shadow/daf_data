0bc11516d8a423643315c62c7c4dedc65d159ab2
ktlim <ktlim@git.lsstcorp.org>
2008-04-02 23:31:40 +0000

    Remove DataProperty.

--- include/lsst/daf/data.h
+++ include/lsst/daf/data.h
@@ -2,12 +2,11 @@
  * This include file will include definitions of classes from the 
  * data sub-module typically needed for definining LsstData objects.
  * This specifically loads definitions for LsstBase and DataProperty, which 
- * intern loads most of all the other LSST support classes.
+ * in turn loads most of all the other LSST support classes.
  */
 #ifndef LSST_DAF_DATA_H
 #define LSST_DAF_DATA_H
 #include "lsst/daf/data/LsstBase.h"
-#include "lsst/daf/data/DataProperty.h"
 #include "lsst/daf/data/SupportFactory.h"
 #include "lsst/daf/data/LsstDataConfigurator.h"
 #endif
